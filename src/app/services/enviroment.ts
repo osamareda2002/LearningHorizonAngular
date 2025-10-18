ab5dbed4c58422a9c182818ec83477e5864486ca
osamareda2002 <osama.reda.abdelghany@gmail.com>
2025-10-18 03:51:26 +0300

    add production url

--- src/app/services/enviroment.ts
+++ src/app/services/enviroment.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: '',
   development: '',
   baseApiUrl: '',
-  horizon: 'https://localhost:7020/api/Horizon',
-  auth: 'https://localhost:7020/api/Auth',
+  horizon: 'https://learninghorizion.runasp.net/api/Horizon',
+  auth: 'https://learninghorizion.runasp.net/api/Auth',
+  // horizon: 'https://localhost:7020/api/Horizon',
+  // auth: 'https://localhost:7020/api/Auth',
 };
